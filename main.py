fb85e7a32ec6d04f38f578530ea6f4c6d6ff2700
slowdh <leok0909@snu.ac.kr>
2023-04-26 17:48:01 +0900

    add frame style transfer code, but not consistent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import torch
+import numpy as np
 from PIL import Image
 from diffusers import StableDiffusionImg2ImgPipeline
 
@@ -9,49 +10,44 @@ model = "prompthero/openjourney-v4"
 prompt = (
     "an vintage impressionist style painting, painted by Claude Monet, oil on canvas"
 )
-video_path = "/Users/leo/Desktop/fun/programming/fast-painter/fast-painter.mp4"
+video_path = "/home/fastdh/server/fast-painter/fast-painter.mp4"
 
 
 pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
     model, torch_dtype=torch.float16
 ).to(device)
-
-image = Image.open(
-    "/home/fastdh/server/fast-painter/test_images/0II7ERB4P6FNXDF5.jpg"
-).convert("RGB")
-image.thumbnail((512, 512))
-
 generator = torch.Generator(device=device).manual_seed(1024)
-image = pipe(
-    prompt=prompt,
-    image=image,
-    strength=0.6,
-    guidance_scale=7.5,
-    generator=generator,
-).images[0]
-
 
 cap = cv2.VideoCapture(video_path)
-
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-assert frame_width == frame_height and frame_width == 512
-
 out = cv2.VideoWriter(
-    filename="output.avi",
-    fourcc=cv2.VideoWriter_fourcc(*"X264"),
+    filename="output.mp4",
+    fourcc=cv2.VideoWriter_fourcc(*"mp4v"),
     fps=24,
-    frameSize=(frame_width, frame_height),
+    frameSize=(512, 512),
 )
-
 
 while True:
     ret, frame = cap.read()
-    out.write(frame)
 
-    input("test")
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    if ret:
+        image = Image.fromarray(frame, 'RGB')
+        image.thumbnail((512, 512))
+
+        image = pipe(
+            prompt=prompt,
+            image=image,
+            strength=0.5,
+            guidance_scale=6,
+            generator=generator,
+        ).images[0]
+        image = np.array(image)
+
+        out.write(image)
+        cv2.imshow('frame',image)
+        input("for debugging")
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
 cap.release()
 out.release()
